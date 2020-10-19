8c0c9bf4483a2fca8ca04d3d9cf9e1bc7763abf4
Qashqyn <qashqyn291@gmail.com>
2020-10-19 13:44:46 +0600

    Create favorite.php

---
+++ favorite.php
@@ -0,0 +1,17 @@
+<?php  
+	$dir = $_SERVER['DOCUMENT_ROOT'];
+	
+	include 'toon.php';
+	$model = new Toon();
+	$toons = $model->fetchFav();
+	$genres = $model->getGenres();
+
+	if (isset($_POST['fav'])) {
+		$toonid = $_POST['toonid'];
+		$model->bookmarkToon($toonid);
+	}
+
+	include 'partials/header.php';
+?>
+<?php include 'partials/toonContainer.php'; ?>
+<?php include 'partials/footer.php'; ?>
