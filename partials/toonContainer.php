df6d21497ee5b2b5a6129d0b5d5bc8538f1065ec
Qashqyn <qashqyn291@gmail.com>
2020-10-20 03:46:12 +0600

    Update toonContainer.php

--- partials/toonContainer.php
+++ partials/toonContainer.php
@@ -1,34 +1,31 @@
 <div class="toon-container">
-	<?php
-	if ($toons && count($toons)>0):
-	$l = count($toons);
-	for ($i = 0; $i < $l/4; $i++): ?>
-		<div class="toon-row">	
-			<?php for ($j=$i*4; $j < $i*4+4 && $j<$l; $j++):
-			$toon = $toons[$j]?>
-			<div class="toon">
-				<i class="toon-bookmark fa <?php echo $toon['fav'] ? "fa-bookmark" : "fa-bookmark-o" ; ?>" aria-hidden="true" data-id="<?php echo $toon['id']; ?>"></i>
-				<a href="/toons/<?php echo $toon['id'] ?>/">
-					<div class="toon-img" style="background-image: <?php echo $toon['img_dir'] ? "url('${toon['img_dir']}" : "url('../../partials/no-img.jpg')"?>">
-						<div class="toon-hover">
-							<p><?php echo $toon['description'] ?></p>
-							<?php  
-								$toonGener = explode(" ", $toon['genre'])[0];
-								$genName = $model->getGenName($toonGener);
-									?>
-								<a class="toon-genre" href="/webtoon-genres.php?genre=<?php echo $toonGener?>"><?php echo $genName?></a>
+		<div class="toons">	
+			<?php if($toons):
+			 foreach ($toons as $toon):?>
+			<div class="toon-block">
+				<div class="toon">
+					<i class="toon-bookmark fa <?php echo $toon['fav'] ? "fa-bookmark" : "fa-bookmark-o" ; ?>" aria-hidden="true" data-id="<?php echo $toon['id']; ?>"></i>
+					<a href="/toons/<?php echo $toon['id'] ?>/">
+						<div class="toon-img" style="background-image: <?php echo $toon['img_dir'] ? "url('${toon['img_dir']}" : "url('../../partials/no-img.jpg')"?>">
+							<div class="toon-hover">
+								<p><?php echo $toon['description'] ?></p>
+								<?php  
+									$toonGener = explode(" ", $toon['genre'])[0];
+									$genName = $model->getGenName($toonGener);
+										?>
+									<a class="toon-genre" href="/webtoon-genres.php?genre=<?php echo $toonGener?>"><?php echo $genName?></a>
+							</div>
 						</div>
-					</div>
-				</a>
+					</a>
 
-				<h3 class="toon-title"><a href="toons/<?php echo $toon['id'] ?>/"><?php echo $toon['name'] ?></a></h3>
+					<h3 class="toon-title"><a href="toons/<?php echo $toon['id'] ?>/"><?php echo $toon['name'] ?></a></h3>
+				</div>
 			</div>
-			<?php endfor ?>
+			<?php endforeach;
+			else: ?>
+				<h2 style="text-align: center;">NO DATA</h2>
+			<?php endif  ?>
 		</div>
-	<?php endfor; 
-	else:?>
-		No data where found
-	<?php endif?>
 
 </div>
 <script src="jquery-3.5.1.min.js"></script>
